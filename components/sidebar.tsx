d3f0229c23a53b5f66dfe4308cca13d36e946e1f
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-10 14:15:09 +0100

    created mobile sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,10 +1,42 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { Home, Plus, Settings } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
 export const Sidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const routes = [
+    { icon: Home, href: "/", label: "Home", isPro: false },
+    { icon: Plus, href: "/companion/new", label: "Home", isPro: true },
+    { icon: Settings, href: "/settings", label: "Settings", isPro: false },
+  ];
+
+  const onNavigate = (url: string, isPro: boolean) => {
+    // TODO: check if pro subscription
+    return router.push(url);
+  };
   return (
     <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
-      <div className="p-3 flex-1 justify-center">
-        <div className="space-y-2">Routes</div>
+      <div className="p-3 flex-1 justify-center flex">
+        <div className="space-y-2">
+          {routes.map((route) => (
+            <div
+              onClick={() => onNavigate(route.href, route.isPro)}
+              key={route.href}
+              className={cn(
+                "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                pathname === route.href && "bg-primary/10 text-primary"
+              )}
+            >
+              <div className="flex flex-col gap-y-2 items-center flex-1">
+                <route.icon className="h-5 w-5" />
+                {route.label}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
