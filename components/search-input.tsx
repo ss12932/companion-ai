4c8f44010eca6cbda6cc17f683453f0beb0d7730
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-19 11:40:05 +0100

    initial search + debounced hook

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,9 +1,27 @@
 "use client";
+import { useState } from "react";
+
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface SearchInputProps {}
 
 const SearchInput: React.FC<SearchInputProps> = ({}) => {
-  return <div>SearchInput</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValue] = useState(name || "");
+
+  return (
+    <div className="relative">
+      <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+      <Input placeholder="Search..." className="pl-10 bg-primary/10" />
+    </div>
+  );
 };
 
 export default SearchInput;
