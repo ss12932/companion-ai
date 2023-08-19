6c370e0c7f5e67c10a26d74a0d734d17182c363b
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-19 12:19:39 +0100

    setup prisma lib file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,1 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+// stop HMR instantiating multiple instances of prisma client
+const prisma = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
+export default prisma;
