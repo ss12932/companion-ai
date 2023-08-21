1826eb7af35ebf769e35ce9a95daa0d9005c0d01
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-21 11:24:43 +0100

    finish tag link buttons

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -4,6 +4,7 @@ import prisma from "@/lib/prisma";
 
 const RootPage = async () => {
   const categories = await prisma.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
