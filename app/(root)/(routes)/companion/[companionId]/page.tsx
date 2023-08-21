334c8f6a06484c5c902f7cbeb2d5e12714e8d65c
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-21 14:01:52 +0100

    started companion form

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,3 +1,6 @@
+import prisma from "@/lib/prisma";
+import CompanionForm from "./components/companion-form";
+
 interface CompanionIdPageProps {
   params: {
     companionId: string;
@@ -6,7 +9,16 @@ interface CompanionIdPageProps {
 
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
   // TODO: check subscription status
-  return <div>Hello Companion Id</div>;
+
+  const companion = await prisma.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prisma.category.findMany();
+
+  return <CompanionForm initialData={companion} categories={categories} />;
 };
 
 export default CompanionIdPage;
