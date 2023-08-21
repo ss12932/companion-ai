8e96a80de1289b1985f1aa153dc15a257ee6c7cc
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-21 17:21:48 +0100

    added empty state logic

---
+++ components/companions.tsx
@@ -0,0 +1,27 @@
+import { Companion } from "@prisma/client";
+import Image from "next/image";
+
+interface CompanionProps {
+  data: (Companion & {
+    _count: {
+      messages: number;
+    };
+  })[];
+}
+
+const Companions: React.FC<CompanionProps> = ({ data }: CompanionProps) => {
+  if (data.length === 0) {
+    return (
+      <div className="pt-10 flex flex-col items-center justify-center space-y-3">
+        <div className="relative w-60 h-60">
+          <Image fill className="grayscale" alt="empty" src="/empty.png" />
+        </div>
+        <p className="text-sm text-muted-foreground">No companions found :(</p>
+      </div>
+    );
+  }
+
+  return <div>Companions</div>;
+};
+
+export default Companions;
