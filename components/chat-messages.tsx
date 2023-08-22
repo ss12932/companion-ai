bb32c1695c3ca77a84c02a15d3b233343ad89947
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-22 21:02:49 +0100

    update stripe model schema

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -43,7 +43,7 @@ const ChatMessages: React.FC<ChatMessagesProps> = ({
           key={message.content}
           role={message.role}
           content={message.content}
-          src={message.src}
+          src={companion.src}
         />
       ))}
       {isLoading && <ChatMessage role="system" src={companion.src} isLoading />}
