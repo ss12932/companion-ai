2e944ac6b8e2048d8439f455206088b488f70729
ss12932 <ss12932@my.bristol.ac.uk>
2023-08-22 22:18:11 +0100

    added to navbar btn pro modal

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,10 +8,12 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 import { MobileSidebar } from "@/components/mobile-sidebar";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 const font = Poppins({ weight: "600", subsets: ["latin"] });
 
 export const Navbar = () => {
+  const proModal = useProModal();
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
@@ -28,7 +30,7 @@ export const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center gap-x-3">
-        <Button size="sm" variant="premium">
+        <Button onClick={proModal.onOpen} size="sm" variant="premium">
           Upgrade
           <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
         </Button>
